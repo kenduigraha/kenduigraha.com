5c69b9b2af80c311467213c7a14d599be7ef0316
Ken Duigraha Putra <kenduigraha@gmail.com>
2025-06-07 14:28:40 +0700

    feat: add style for header and footer.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,10 @@
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
 document.getElementById('contactForm').addEventListener('submit', function (e) {
   e.preventDefault();
 
